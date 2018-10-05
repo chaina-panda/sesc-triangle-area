5332724d3fe1447967c9189628b10a54e73ada76
chaina-panda <chaina-panda@mail.ru>
2018-10-05 20:00:06 +0700

    sesc-triangle-area
    sesc-triangle-area

---
+++ Makefile
@@ -0,0 +1,9 @@
+PROGRAM= sesc-triangle-area
+
+run: ${PROGRAM}
+./${PROGRAM}
+
+${PROGRAM}: ${PROGRAM}.c
+gcc -Wall -std=c11 ${PROGRAM}.c -o ${PROGRAM}
+
+build: ${PROGRAM}
---
+++ sesc-triangle-area.c
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdio.h>
+int main() {
+double a, Tri;
+scanf("%lf", &a);
+if (a<=1000) {Tri = sqrt(3)/4*a*a;
+    printf("%.3lf", Tri);
+ } else {};
+return 0;
+}
